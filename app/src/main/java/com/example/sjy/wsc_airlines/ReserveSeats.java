13bcbb71d2ae30fe0eaba3b85cc7e4afc481a589
sunjy <sunjy00123@163.com>
2018-04-07 21:10:37 +0800

    增加了半成品的选座部分

---
+++ app/src/main/java/com/example/sjy/wsc_airlines/ReserveSeats.java
@@ -0,0 +1,13 @@
+package com.example.sjy.wsc_airlines;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReserveSeats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_seats);
+    }
+}
